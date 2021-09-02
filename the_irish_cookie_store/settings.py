1ff54517ea7a839bf1af3872b60e5505c6becc32
ByteSizedDev <bytesizeddev@gmail.com>
2021-09-02 08:10:49 +0000

    Removed secret key and set debug

--- the_irish_cookie_store/settings.py
+++ the_irish_cookie_store/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0cvb@dq&wc4@h4wa*+w47o+p57j9ov7_5jct=-hyn$$(8u00&o'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-irish-cookie-store.herokuapp.com', 'localhost']
 
