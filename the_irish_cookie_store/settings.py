51d986daa5b20b4e30158eff1c1a7a1f0de173af
rossph3lan <rossph3lan@gmail.com>
2022-01-17 02:42:12 +0000

    Changed text on home page

--- the_irish_cookie_store/settings.py
+++ the_irish_cookie_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['the-irish-cookie-store.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['the-irish-cookie-store.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
